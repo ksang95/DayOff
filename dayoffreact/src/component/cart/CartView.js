78ae325ab3b96bbf6b29ccc09f8263db4d62504a
yunju <dbswnha@gmail.com>
2019-12-30 14:21:47 +0900

    상품상세 수정

--- dayoffreact/src/component/cart/CartView.js
+++ dayoffreact/src/component/cart/CartView.js
@@ -3,25 +3,17 @@ import axios from 'axios';
 import Cart from './Cart';
 
  class CartView extends Component {
+
   state = {
    cartView:[]
   ,realTotal:0
   }
-  // handleCartView() {
-  //   const params = new URLSearchParams();
-  //   params.append("userId",1);
-  //   axios({
-  //     method: "post",
-  //     url: "/cartView",
-  //     data: params
-  //   }).then(res => {
-  //     console.log(res);
-  //     this.setState({
-  //       cartView: res.data
-  //     });
-  //   });
-  // }
-  handleCartView() {
+
+
+
+  handleCartView = () => {
+    const userId = sessionStorage.getItem("userId")
+    if(userId !== null){
       axios.get('/cartList',{params:{userId:sessionStorage.getItem("userId")}})
      .then(res => {
       
@@ -34,21 +26,60 @@ import Cart from './Cart';
     cartView:[],
     realTotal:this.state.realTotal +this.state.cartView.totalPrice
   })
-  } 
-// getRealTotal(){
-//   this.setState({
-//     cartView:[],
-//     realTotal:this.state.realTotal +this.state.cartView.totalPrice
-//   })
-// }
+    }else{
+      const output =localStorage.getItem("cart1");
+      const cart1 = JSON.parse(output);
+      console.log(cart1);
+      this.setState({cartView:this.state.cartView.concat(cart1),
+      
+      });
+      this.setState({
+        realTotal:this.state.realTotal + this.state.cartView.totalPrice
+      })
+    }
+}
+DeleteAll(){
+  const userId = sessionStorage.getItem("userId")
+  if(userId !== null){
+  console.log(this.state.cartView.id)
+  axios.post('/deleteCartItem',this.state.cartView.id)
+  .then(res=> {
+    this.setState({
+      cartView:res.data
+    })
+  })}else{
+  console.log(this.state.cartView.id)
+
+    localStorage.clear();
+  }
+}
+
+handleDeleteItem(){
+  const userId = sessionStorage.getItem("userId")
+  if(userId !== null){
+  console.log(this.state.cartView.id)
+  axios.post('/deleteCartItem',this.state.cartView.id)
+  .then(res=> {
+    this.setState({
+      cartView:res.data
+    })
+  })}else{
+  console.log(this.state.cartView.id)
+
+    localStorage.clear();
+  }
+}
+
   componentDidMount(){
     this.handleCartView();
   }
   render() {
    
     const cartView = this.state.cartView.map( cart => {
+      console.log(cart)
       return <Cart
-      key={cart.id}
+      key={cart.id} 
+      productImage={"https://storage.googleapis.com/bit-jaehoon/"+cart.productImage}
       productThumbnailName={"https://storage.googleapis.com/bit-jaehoon/"+cart.productThumbnailName}
       name={cart.name}
       color={cart.color}
@@ -57,9 +88,7 @@ import Cart from './Cart';
       price={cart.price}
       totalPrice={cart.totalPrice}
       realTotal={cart.realTotal}
-      //getRealTotal={cart.getRealTotal}
-      handleCartView={cart.handleCartView}
-      
+      onClick={this.state.handleDeleteItem}
     />;
     });
     return (
@@ -73,7 +102,9 @@ import Cart from './Cart';
             <col style={{width: + 10+'%'}}></col>
             <col style={{width: + 10+'%'}}></col>
             <col style={{width: + 10+'%'}}></col>
+            <col style={{width: + 10+'%'}}></col>
           </colgroup>
+          <tr onClick={this.state.DeleteAll}>전체삭제</tr>
           <tr >
               <th>상품정보</th>
               <th>색깔</th>
@@ -81,9 +112,11 @@ import Cart from './Cart';
               <th>수 량</th>
               <th>금액</th>
               <th>총금액</th>
+              <th>단일상품 삭제</th>
           </tr>
           <tbody>
       {cartView}
+    
         </tbody>
         </table>
 
