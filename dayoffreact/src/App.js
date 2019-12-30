707bacbcf6c7f63bee542863842d8b6d0a790d5f
ksang95 <ksanghe2@naver.com>
2019-12-30 12:47:03 +0900

    상희: app.js 업뎃

--- dayoffreact/src/App.js
+++ dayoffreact/src/App.js
@@ -2,30 +2,29 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import OrderAnalysis from './component/admin/analysis/OrderAnalysis';
 import UserAnalysis from './component/admin/analysis/UserAnalysis';
+import Orders from './component/admin/orders/orders';
 import UpdateInvoice from './component/admin/orders/updateInvoice';
-import Menu from './component/admin/product/Menu';
 import ProductAdd from './component/admin/product/ProductAdd';
+import ProductUpdate from './component/admin/product/ProductUpdate';
 import UserAxios from './component/admin/userList/UserAxios';
+import CartView from "./component/cart/CartView";
+import Footer from './component/common/footer/Footer';
+import Header from './component/common/header/Header.js';
+import deny from './component/common/login/deny';
+import ErrorPage from './component/common/login/ErrorPage';
+import LoginPage from './component/common/login/LoginPage';
 import LoginSuccess from './component/common/login/LoginSuccess';
 import SignUp from './component/common/login/SignUp';
-import Withdraw from './component/myPage/user/Withdraw';
+import Main from './component/main/Main';
+import Myorders from './component/myPage/orders/Myorders';
 import OrderDetail from './component/myPage/orders/orderDetail/OrderDetail';
 import RefundRequest from './component/myPage/orders/RefundRequest';
 import UpdateUser from './component/myPage/user/UpdateUser';
-import Orders from './component/admin/orders/orders';
-import Vision from './component/vision/vision';
-import Myorders from './component/myPage/orders/Myorders'
-import StopProductSale from './component/admin/product/StopProductSale';
-import Header from './component/common/header/Header.js'
-import Main from './component/main/Main';
-import Footer from './component/common/footer/Footer';
-import LoginPage from './component/common/login/LoginPage';
-import ErrorPage from './component/common/login/ErrorPage';
-import CartView from "./component/cart/CartView";
+import Withdraw from './component/myPage/user/Withdraw';
 import ProductInfo from './component/productDetail/product/ProductInfo';
+import Vision from './component/vision/vision';
 import { ProtectedRoute } from "./ProtectedRoute";
-import {ProtectedRouteAdmin} from "./ProtectedRouteAdmin"
-import deny from './component/common/login/deny';
+import { ProtectedRouteAdmin } from "./ProtectedRouteAdmin";
 class App extends Component {
   render() {
     return (
@@ -41,7 +40,7 @@ class App extends Component {
           <Route path="/error" component={ErrorPage} />
           <ProtectedRouteAdmin path="/admin/updateInvoice" component={UpdateInvoice} />
           <ProtectedRouteAdmin path="/admin/addProduct" component={ProductAdd} />
-          <ProtectedRouteAdmin path="/admin/stopProductSale/:productId" component={StopProductSale} />
+          <ProtectedRouteAdmin path="/admin/updateProduct/:productId" component={ProductUpdate} />
           <ProtectedRouteAdmin path="/admin/userList" component={UserAxios} />
           <ProtectedRouteAdmin path="/admin/usersAnalysis" component={UserAnalysis} />
           <ProtectedRouteAdmin path="/admin/ordersAnalysis" component={OrderAnalysis} />
