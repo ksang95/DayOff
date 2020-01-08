de78ee1b8c9941ca20418148957796ca2d4295f6
ksang95 <ksanghe2@naver.com>
2020-01-08 15:44:58 +0900

    상희:우체국택배로

--- dayoffreact/src/component/myPage/orders/orderDetail/OrderType.js
+++ dayoffreact/src/component/myPage/orders/orderDetail/OrderType.js
@@ -8,7 +8,7 @@ class OrderType extends Component {
 
 
     onClick = () => {
-        window.open("https://tracker.delivery/#/kr.cjlogistics/" + this.props.info.invoice, '', 'width=700px, height=500px, left=500px ,top=200px');
+        window.open("https://tracker.delivery/#/kr.epost/" + this.props.info.invoice, '', 'width=700px, height=500px, left=500px ,top=200px');
     }
     render() {
         const { info } = this.props;
