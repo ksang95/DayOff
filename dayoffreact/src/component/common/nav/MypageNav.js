7e8203a80cb54aab42ea26e0d08c3daa1f8b2590
ksang95 <ksanghe2@naver.com>
2020-01-09 13:18:36 +0900

    상희:카테고리 css 수정+카테고리명 수정

--- dayoffreact/src/component/common/nav/MypageNav.js
+++ dayoffreact/src/component/common/nav/MypageNav.js
@@ -8,19 +8,19 @@ export default class Nav extends Component {
     return (
       <div className="Nav">
         <div className='Nav_Title'>
-        <h3><img className='Face' src='/images/face.png'></img> My PAGE</h3>
+        <h3>My PAGE</h3>
         </div>
         <div className='Nav_Info1'>
-          <h5>쇼핑정보</h5>
-          <NavLink to={'/mypage/myorders'} className='Nav_Info_List'>주문내역/환불/반품</NavLink><br></br>
-          <NavLink to={'/mypage/map'} className='Nav_Info_List'>매장 픽업 서비스</NavLink><br></br>
-          <NavLink to={'/mypage/myGrade'} className='Nav_Info_List'>회원등급 현황</NavLink>
+          <h5>쇼핑 정보</h5>
+          <p><NavLink to={'/mypage/myorders'} className='Nav_Info_List'>주문 내역</NavLink></p>
+          <p><NavLink to={'/mypage/map'} className='Nav_Info_List'>매장 픽업 서비스</NavLink></p>
+          <p><NavLink to={'/mypage/myGrade'} className='Nav_Info_List'>회원 등급 현황</NavLink></p>
         </div>
 
         <div className='Nav_Info2'>
-          <h5>나의정보</h5>
-          <NavLink to={'/mypage/myInfo'} className='Nav_Info_List'>회원정보 관리</NavLink><br></br>
-          <NavLink to={'/mypage/withdraw'} className='Nav_Info_List'>회원탈퇴</NavLink>
+          <h5>나의 정보</h5>
+          <p><NavLink to={'/mypage/myInfo'} className='Nav_Info_List'>회원 정보 수정</NavLink></p>
+          <p><NavLink to={'/mypage/withdraw'} className='Nav_Info_List'>회원 탈퇴</NavLink></p>
         </div>
 
           <img className='tphone' src='/images/tphone1.png'></img>
